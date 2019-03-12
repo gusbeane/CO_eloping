9cf00ebf73e65b04a9a163d6b6c868c40d573f7c
gusbeane <abeane@sas.upenn.edu>
2019-03-12 15:38:10 -0400

    Add chisq function

--- fisher_plots/threefield.py
+++ fisher_plots/threefield.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import chi2
 
 def gen_Vk(kmax, Vsurv):
     ans = kmax**3/(6.*np.pi**2)
@@ -45,4 +46,34 @@ def gen_fisher_matrix(B1, B2, B3, N1, N2, N3, kmax, Vsurv):
     mat = Vk * np.array([[F11, F12, F13], [F21, F22, F23], [F31, F32, F33]])
 
     return mat
+
+class chisq(object):
+    def __init__(self, B1, B2, B3, cov_mat):
+        self.B1 = B1
+        self.B2 = B2
+        self.B3 = B3
+
+        self.cov_mat = cov_mat
+
+        # self.sigma1 = np.sqrt(cov_mat[0][0])
+        # self.sigma2 = np.sqrt(cov_mat[1][1])
+        # self.sigma3 = np.sqrt(cov_mat[2][2])
+
+        # self.sigma12 = cov_mat[0][1]
+        # self.sigma23 = cov_mat[1][2]
+        # self.sigma31 = cov_mat[2][0]
+
+        # self.rho12 = self.sigma12/(self.sigma1*self.sigma2)
+        # self.rho23 = self.sigma23/(self.sigma2*self.sigma3)
+        # self.rho31 = self.sigma31/(self.sigma3*self.sigma1)
+
+    def __call__(self, b1, b2, b3, chisq):
+        d1 = b1 - self.B1
+        d2 = b2 - self.B2
+        d3 = b3 - self.B3
+        d = np.array([d1, d2, d3])
+
+        chisq = np.matmul(np.matmul(np.transpose(d), np.linalg.inv(self.cov_mat)), d)
+        
+        return chisq
 
