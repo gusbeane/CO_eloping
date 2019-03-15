50302efd202da04490e455329c025fb54faca32e
gusbeane <abeane@sas.upenn.edu>
2019-03-15 16:10:21 -0400

    Fix imports, add cosmo

--- fisher_plots/CO_predictions_threefield.py
+++ fisher_plots/CO_predictions_threefield.py
@@ -1,5 +1,9 @@
 import numpy as np 
 
-from threefield import gen_fisher_matrix
+from threefield import gen_fisher_matrix, gen_Vk
+import CO_data
+from colossus.cosmology import cosmology
 
+params = {'flat': True, 'H0': 70, 'Om0': 0.27, 'Ob0': 0.046, 'sigma8': 0.8, 'ns': 1.0}
+cosmo = cosmology.setCosmology('myCosmo', params)
 
