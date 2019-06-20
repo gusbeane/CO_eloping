893c3c761b37bd7c726c9e1b78d9f8c6d2a15b1c
gusbeane <abeane@sas.upenn.edu>
2019-06-20 13:03:51 -0400

    Add multifield, check for assigned bias (for now)

--- multifield.py
+++ multifield.py
@@ -4,4 +4,11 @@ import itertools
 from scipy.special import gamma
 import CO_data
 
+class multifield(object):
+    def __init__(self, z, line_list):
 
+        self._check_assigned_bias_(line_list)
+    
+    def _check_assigned_bias_(self, line_list):
+        for line in line_list:
+            assert line.bias_assigned, "Bias not assigned for line "+line.key
